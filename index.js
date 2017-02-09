0b96bf20d8976e36057c44cda967fc6cb68a8c31
0x4d <0x4d@undefined>
2017-02-09 17:05:57 +0100

    Connect to database

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const Mongorito = require('mongorito')
+
+Mongorito.connect(`${process.env.MONGODB_HOST}/cloudnet`).then(
+  connection => {
+    console.log('Connected to database!')
+  },
+  error => {
+    console.error(error)
+  }
+)
+
 const app = require('koa')()
 
 app.use(function * () {
